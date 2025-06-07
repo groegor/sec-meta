cb2352e9310735d6eb90d050691a80b4aff0bd7e
Philipp Groegor <philipp.groegor@gmail.com>
2025-06-07 11:48:52 +0200

    Updated package build

--- build.py
+++ build.py
@@ -1,10 +1,14 @@
 import subprocess
 import sys
+import os
 
 def build_package():
     """Build the package"""
     subprocess.check_call([sys.executable, "-m", "pip", "install", "--upgrade", "pip"])
     subprocess.check_call([sys.executable, "-m", "pip", "install", "build", "twine"])
+    
+    # Change to the directory containing pyproject.toml
+    os.chdir("secmeta_pkg")
     subprocess.check_call([sys.executable, "-m", "build"])
     
 if __name__ == "__main__":
